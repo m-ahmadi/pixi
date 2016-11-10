d3d248ca098d5db2b9aed2f0cd0d51b0dcddc742
mohammad-ahmadi <mohammad.ahmadi1989@yahoo.com>
2016-11-10 16:56:15 +0330

    4

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {
-	backgroundColor: 0x1099bb
+	backgroundColor: 0x1099bb,
+	id: 'abc'
 });
 document.body.appendChild(renderer.view);
 
@@ -11,25 +12,35 @@ var texture = PIXI.Texture.fromImage('images/computer.png');
 var background = PIXI.Sprite.fromImage('images/hard-drive.png');
 background.interactive = true;
 background.buttonMode = true;
+background.x = 100;
+background.y = 250;
+background.anchor.set(0, 0);
+background.scale.set(0.5);
+
 background
 	.on('mousedown', ds)
 	.on('mouseup', de)
 	.on('mouseupoutside', de)
 	.on('mousemove', dm);
-
-var ds = function ds(e) {
+	
+//var newPosition;
+function ds(e) {
+	//newPosition = e.data.getLocalPosition(this.parent);
 	this.data = e.data;
 	this.alpha = 0.5;
 	this.dragging = true;
+	
+	//@@@console.log( e.data.global.x - this.x );
 };
-var dm =  function () {
+function dm(e) {
 	if (this.dragging) {
-		var newPosition = this.data.getLocalPosition(this.parent);
+		var newPosition = e.data.getLocalPosition(this.parent);
+		
 		this.position.x = newPosition.x;
 		this.position.y = newPosition.y;
 	}
 };
-var de =   function () {
+function de() {
 	this.alpha = 1;
 	this.dragging = false;
 	this.data = null;
@@ -39,7 +50,7 @@ var sprite = new PIXI.Sprite(texture);
 sprite.interactive = true;
 sprite.buttonMode = true;
 sprite.anchor.set(0.5);
-sprite.scale.set(0.06);
+sprite.scale.set(0.1);
 sprite
 	.on('mousedown', dragstart)
 	.on('touchstart', dragstart)
@@ -49,10 +60,13 @@ sprite
 	.on('touchendoutside', dragend)
 	.on('mousemove', dragmove)
 	.on('touchmove', dragmove);
+
 sprite.position.x = 200;
 sprite.position.y = 20;
 
 var rect = new PIXI.Graphics();
+rect.interactive = true;
+rect.buttonMode = true;
 rect.beginFill(0x0033CC);
 rect.lineStyle(4, 0xFF0000, 1);
 rect.drawRect(0, 0, 96, 96);
@@ -61,7 +75,12 @@ rect.x = 64;
 rect.y = 64;
 rect.alpha = 0.5;
 
-var text = new PIXI.Text("Hello Pixi!", {font: "48px Impact", fill: "red"});
+rect
+	.on('mousedown', ds)
+	.on('mouseup', de)
+	.on('mouseupoutside', de)
+	.on('mousemove', dm);
+var text = new PIXI.Text("salam", {font: "24px Impact", fill: "red"});
 
 stage.addChild(background);
 stage.addChild(rect);
@@ -74,6 +93,8 @@ function animate() {
     requestAnimationFrame(animate);
     renderer.render(stage);
 }
+
+
 function dragstart(e) {
 	console.log(this);
     this.data = e.data;
@@ -93,3 +114,55 @@ function dragend() {
     this.dragging = false;
     this.data = null;
 }
+
+var initX,
+	initY,
+	left = false,
+	right = false,
+	up = false,
+	down = false;
+	
+$('canvas')
+	.on('mousedown', function (e) {
+		this.dragit = true;
+		initX = e.pageX;
+		initY = e.pageY
+	})
+	.on('mousemove', function (e) {
+		var x = e.pageX,
+			y = e.pageY;
+		
+		if (!this.dragit) { return; }
+		
+		
+		if (x < initX) { // left
+			right = false;
+			left= true;
+		} else if (x > initX) { // right
+			right = true;
+			left = false;
+		}
+		if (y < initY) { // up
+			down = false;
+			up = true;
+		} else if (y >  initY) { // down
+			up = false;
+			down = true
+		}
+		
+		if (left) {
+			stage.x += 1;
+		}
+		if (right) {
+			stage.x -= 1;
+		}
+		if (up) {
+			stage.y += 1;
+		}
+		if (down) {
+			stage.y -= 1;
+		}
+	})
+	.on('mouseup', function () {
+		this.dragit = false;
+	});
--- js/ten-k.js
+++ js/ten-k.js
@@ -1,6 +1,6 @@
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // arz va ertefa monitoretoon ro bas dasti bezanin to line 10 va 11
-var nodes = 10000,   // kole noda
+var nodes = 30000,   // kole noda
 	width = 1920,    // arz safe
 	height = 950,    // ertefa safe
 	nodeSize = 0.04, // andaze noda
@@ -20,12 +20,15 @@ for (var i = 0; i < nodes; i+=1) {
 	createBunny(Math.floor(Math.random() * 1920) , Math.floor(Math.random() * 950));
 }
 
+
+
+
 function createBunny(x, y) {
     var bunny = new PIXI.Sprite(texture);
 
     bunny.interactive = true;
     bunny.buttonMode = true;
-    bunny.anchor.set(0.5);
+    bunny.anchor.set(0.5, 0.5);
     bunny.scale.set(nodeSize);
 
     bunny
@@ -42,8 +45,33 @@ function createBunny(x, y) {
 
 	bunny.position.x = x;
 	bunny.position.y = y;
-
 	stage.addChild(bunny);
+/*
+	var rect = new PIXI.Graphics();
+	rect.interactive = true;
+	rect.buttonMode = true;
+	rect.beginFill(0x0033CC);
+	rect.lineStyle(4, 0xFF0000, 1);
+	rect.drawRect(0, 0, 36, 36);
+	rect.endFill();
+	rect.alpha = 0.5;
+	 rect
+		.on('mousedown', onDragStart)
+		.on('touchstart', onDragStart)
+
+		.on('mouseup', onDragEnd)
+		.on('mouseupoutside', onDragEnd)
+		.on('touchend', onDragEnd)
+		.on('touchendoutside', onDragEnd)
+		
+		.on('mousemove', onDragMove)
+		.on('touchmove', onDragMove);
+
+	rect.position.x = x;
+	rect.position.y = y;
+	
+	stage.addChild(rect);
+*/
 }
 
 requestAnimationFrame( animate );
@@ -70,3 +98,71 @@ function onDragMove() {
 		this.position.y = newPosition.y;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var initX,
+	// initY,
+	// left = false,
+	// right = false,
+	// up = false,
+	// down = false;
+// $('canvas')
+	// .on('mousedown', function (e) {
+		// this.dragit = true;
+		// initX = e.pageX;
+		// initY = e.pageY
+	// })
+	// .on('mousemove', function (e) {
+		// var x = e.pageX,
+			// y = e.pageY;
+		
+		// if (!this.dragit) { return; }
+		
+		
+		// if (x < initX) { // left
+			// right = false;
+			// left= true;
+		// } else if (x > initX) { // right
+			// right = true;
+			// left = false;
+		// }
+		// if (y < initY) { // up
+			// down = false;
+			// up = true;
+		// } else if (y >  initY) { // down
+			// up = false;
+			// down = true
+		// }
+		
+		// if (left) {
+			// stage.x -= 50;
+		// }
+		// if (right) {
+			// stage.x += 50;
+		// }
+		// if (up) {
+			// stage.y -= 50;
+		// }
+		// if (down) {
+			// stage.y += 50;
+		// }
+	// })
+	// .on('mouseup', function () {
+		// this.dragit = false;
+	// });
