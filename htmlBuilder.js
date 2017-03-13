8a1f795c4c1dc2e82b66424c10d49487c7f71d31
mohammad-ahmadi <mohammad.ahmadi1989@yahoo.com>
2017-03-13 16:02:36 +0330

    htmlBuilder

--- htmlBuilder.js
+++ htmlBuilder.js
@@ -7,13 +7,13 @@ let watcher = chokidar.watch(`${DIR}/**/*${EXT}`, {ignored: /[\/\\]\./, persiste
 let colors = require("colors/safe");
 let fs = require("fs");
 let fileOpt = {encoding: "utf-8", flag: "r"};
-let once = process.argv[2] === "--once";
+let once = process.argv.indexOf("--once") !== -1;
 let log = console.log;
 
 if (!once) {
 	watcher
 		.on("ready", function () {
-			log(colors.blue.bold("Initial scan complete. Ready for changes."));
+			log( colors.blue.bold("Initial scan complete. Ready for changes.") );
 		})
 		.on("add", path => {
 			log( colors.green.bold("File added:"), path );
@@ -51,48 +51,127 @@ function walk(dir) {
 function readFile(path) {
 	return fs.readFileSync(path, { encoding: 'utf-8', flag: 'r' });
 }
-function getDirsIn(p) {
+function dirs(p) {
 	return fs.readdirSync(p).filter( f => fs.statSync(p+"/"+f).isDirectory() );
 }
-function getFilesIn(p) {
-	return p => fs.readdirSync(p).filter( f => !fs.statSync(p+"/"+f).isDirectory() );
+function files(p) {
+	return fs.readdirSync(p).filter( f => fs.statSync(p+"/"+f).isFile() );
 }
+
+let src = {
+	template: undefined,
+	data: {}
+};
+/*
+	{
+		template: fn(data),
+		data: {
+			profile: "",
+			modals: "",
+			box: "",
+		},
+	
+	}
+*/
+
+function addTemplate(path, namespace) {
+	if (namespace) {
+		src[namespace].template = Handlebars.compile( readFile(path) );
+	} else {
+		src.template = Handlebars.compile( readFile(path) );
+	}
+}
+function addData(path, namespace, fileName) {
+	if (namespace) {
+		src[namespace].data[fileName] = readFile(path);
+	} else {
+		src.data[fileName] = readFile(path);
+	}
+}
+
+function fudge(path, namespace) {
+	let root = path+"/";
+	
+	files(path).forEach(i => {
+		let fullPath = root+i;
+		if ( i.endsWith(".handlebars") ) {
+			addTemplate(fullPath, namespace);
+		} else if ( i.endsWith(".htm") ) {
+			let fileName = i.substr( 0, i.indexOf('.') );
+			addData(fullPath, namespace, fileName);
+		}
+	});
+	["a.htm", "b.htm", "c.asad"]
+	dirs(path).forEach(i => {
+		let fullPath = root+i;
+		console.log( fullPath );
+		let files = files(fullPath);
+		
+		if (files.indexOf("main.handlebars") !== -1) { // folder contains .handlebars
+		
+		} else { // folder doesn't contain .handlebars
+			if (files.length) {
+				files.forEach(i => {
+					if ( i.endsWith("htm") ) {
+						addData( fullPath, i, i.substr( 0, i.indexOf('.') ) );
+					}
+				});
+			} else {
+				fudge(fullPath, i);
+			}
+		}
+	});
+	
+	console.log(src);
+}
+function createIndex() {
+	fudge("template/static/modals");
+	
+	
+}
+
+
+createIndex();
+
+/*
 
 function createIndex() {
 	let index = readFile(DIR+"/index.handlebars");
 	
-	walk(DIR).forEach(i => {
+	let output = "";
+	
+	console.log( files(DIR) );
+	walk("template/static/profile").forEach(i => {
 		let p = i.replace(DIR, "").slice(1);
 		let a = p.split("/");
-		let last = a[a.length-1];
-		// console.log(a);
-		if (last.endsWith(".handlebars")) {
-			// console.log(last.replace(DIR, ""));
+		
+		if (p.indexOf("/") === -1) { // root folder
+			src.data = {};
+			if ( p.endsWith(".handlebars") ) { // template
+				
+				
+				src.template = Handlebars.compile( readFile(i) )();
+				
+			} else if ( p.endsWith(".htm") ) { // data
+				let fileName = p.substr( 0, p.indexOf('.') );
+				src.data[fileName] = readFile(i);
+				
+				
+			}
+		} else if (p.indexOf("/") >=0) { // 
+			
 		}
 	});
-	console.log( walk(DIR) );
+	// console.log( walk(DIR) );
 	
 	
 	
 	
 	
 	
-	/* let source = readFile(DIR+"/main"+EXT);
-	let a = Handlebars.compile(source);
-	
-	console.log(a);
-	fs.writeFile("shindex.html", "Hey there!", "utf8", (err) => {
-		if (err) { return console.log(err); }
-		log("The file was saved!");
-	}); */
 }
 
-createIndex();
 
+if (files.filter(v => {return /.htm/.test(v)}).length) { // at-least one .htm file
 
-/*
-let g = Handlebars.compile(src.main)();
-	console.log(g);
-	// getDirsIn(DIR).forEach(i => {
-	
 */
